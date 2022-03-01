c9b0f5bed7780ece1770df30edf0cb35f81fab1b
Fernando Herrera <fherrera0206@gmail.com>
2022-03-01 12:24:01 -0500

    Create a route with navlink and active class

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 function App() {
   return (
@@ -10,9 +13,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" />
-          <Route path="/missions" />
-          <Route path="/profile" />
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import logo from '../img/planet.png';
-import { Link } from 'react-router-dom';
-import './header.css';
+import { NavLink } from 'react-router-dom';
+
+const navLinkStyles = ({ isActive }) => {
+    return {
+        fontWeight: isActive ? 'bold' : "normal",
+        textDecoration: isActive ? "underline" : "none",
+    }
+}
 
 const Header = () => (
     <header className="d-flex justify-content-between container align-items-center my-2 border-bottom pb-2">
@@ -8,12 +14,11 @@ const Header = () => (
             <img src={logo} alt="logo" width="50px" />
             <h1>Space Traveler's Hub</h1>
         </div>
-        <nav className="d-flex gap-3">
-            <Link to={"/"}>Rockets</Link>
-            <Link to={"/missions"}>Missions</Link>
-            <Link to={"/profile"}>My Profile</Link>
+        <nav className="d-flex gap-3 text-decoration-none">
+            <NavLink style={navLinkStyles} to={"/"}>Rockets</NavLink>
+            <NavLink style={navLinkStyles} to={"/missions"}>Missions</NavLink>
+            <NavLink style={navLinkStyles} to={"/profile"}>My Profile</NavLink>
         </nav>
-        <hr class="bg-danger border-2 border-top border-danger" />
     </header>
 );
 
