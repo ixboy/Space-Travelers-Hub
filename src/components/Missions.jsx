b242cef62839495649d25e2ad04221b331f0654b
Fernando Herrera <fherrera0206@gmail.com>
2022-03-02 11:59:53 -0500

    Create a table with Bootstrap

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,7 +1,44 @@
-const Missions = () => (
-  <div>
-    <h1>Missions</h1>
-  </div>
-);
+import { useSelector } from "react-redux";
+import { Table } from "react-bootstrap";
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+
+  return (
+    <div>
+      <h1>Missions</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+
+    </div>
+  );
+};
 
 export default Missions;
