a84e9aecf46bc8b1c1b6667c8888c245560258a3
Fernando Herrera <fherrera0206@gmail.com>
2022-03-01 21:54:24 -0500

    Fix Provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './redux/store';
+// import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <div>
       <App />
-    </Provider>
+    </div>
   </React.StrictMode>,
   document.getElementById('root'),
 );
