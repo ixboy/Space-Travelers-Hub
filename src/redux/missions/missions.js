1e151d4fc34f6c74f4926deb4b09dfd8585c52a2
Fernando Herrera <fherrera0206@gmail.com>
2022-03-02 10:07:04 -0500

    Create actions and reducers to missions

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,34 @@
+const initialState = [];
+
+const GET_DATA = "SPACE_TRAVELERS_HUB/MISSIONS/GET_DATA";
+
+const url = "https://api.spacexdata.com/v3/missions";
+
+export const addMissions = (payload) => ({
+  type: GET_DATA,
+  payload,
+});
+
+export const getMissions = async (dispatch) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  data.map(element => ({
+    id: element.mission_id,
+    name: element.mission_name,
+    description: element.description,
+  }));
+  console.log(data);
+  dispatch(addMissions(data));
+}
+
+const missionsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_DATA:
+      return [...state, action.payload];
+
+    default:
+      return state;
+  }
+}
+
+export default missionsReducer;
