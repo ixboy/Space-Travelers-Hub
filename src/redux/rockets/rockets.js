f147fa2dc7c4fecdaa947c216a7250ebe519361b
ixboy <ismaelixboy@gmail.com>
2022-03-04 00:19:42 +0300

    create rockets reducer and action creators

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -21,3 +21,35 @@ export const cancelBooking = (id) => ({
   id,
 });
 
+const rocketsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ROCKETS:
+      return { ...state, rockets: action.rockets };
+    case BOOK_ROCKET:
+      return {
+        ...state,
+        rockets: state.rockets.map((rocket) => {
+          if (rocket.id !== action.id) { return rocket; }
+          return { ...rocket, reserved: true };
+        }),
+      };
+    case CANCEL_BOOKING:
+      return {
+        ...state,
+        rockets: state.rockets.map((rocket) => {
+          if (rocket.id !== action.id) { return rocket; }
+          return { ...rocket, reserved: false };
+        }),
+      };
+    default:
+      return state;
+  }
+};
+
+export const fetchRockets = () => async (dispatch) => {
+  const action = { type: GET_ROCKETS };
+  const rockets = await apiGetElements(action);
+  dispatch(getRockets(rockets));
+};
+
+export default rocketsReducer;
