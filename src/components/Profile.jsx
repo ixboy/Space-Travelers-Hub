644bf35f8071d32da356ad304fb526ced46d54a7
ixboy <ismaelixboy@gmail.com>
2022-03-04 03:33:17 +0300

    create and organize profile data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,37 +1,45 @@
-import { useSelector } from "react-redux";
-import { Table } from "react-bootstrap";
+// import ListGroup from 'react-bootstrap/ListGroup';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => {
-  const missions = useSelector((state) => state.missionsReducer);
-  const reservedMissions = missions.filter((mission) => mission.reserved === true);
-  // console.log(missions);
+const MyProfile = () => {
+  const joinedMissions = useSelector((s) => s.missionsReducer.filter((m) => m.reserved));
+  const joinedRockets = useSelector((s) => s.rocketsReducer.rockets.filter((r) => r.reserved));
+
   return (
-    <div className="container d-flex gap-2 justify-content-around">
-
-      {reservedMissions.length ? (
+    <div className="container d-flex gap-2 justify-content-around align-items-center vh">
+      {joinedMissions.length ? (
         <div>
-          <h2>My Missions</h2>
-          <ul>
-            {reservedMissions.map((item) => (
-              <li key={item.id}>{item.name}</li>
+          <h2 className="my-4">My Missions</h2>
+          <ul className="d-flex list-style gap-3 flex-column m-0 p-0">
+            {joinedMissions.map((item) => (
+              <li className="py-3 px-5 border text-center rounded" key={item.id}>{item.name}</li>
             ))}
           </ul>
         </div>
       ) : (
-        <div>
-          <h2>My Missions</h2>
-          <h4>No missions reserved</h4>
+
+        <div className="border border-2 rounded">
+          <h4 className="p-3">No missions reserved</h4>
         </div>
-
       )}
+      {joinedRockets.length ? (
+        <div>
+          <h2 className="my-4">My Rockets</h2>
+          <ul className="d-flex list-style gap-3 flex-column m-0 p-0">
+            {joinedRockets.map((item) => (
+              <li className="py-3 px-5 border text-center rounded" key={item.id}>{item.rocket_name}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
 
-      <div>
-        <h2>My Rockets</h2>
-      </div>
-
+        <div className="border border-2 rounded">
+          <h4 className="p-3">No Rockets</h4>
+        </div>
+      )}
     </div>
   );
-
 };
 
-export default Profile;
+export default MyProfile;
